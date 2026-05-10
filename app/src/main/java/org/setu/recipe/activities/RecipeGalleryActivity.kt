2358966c56a3148f67b07e22ce7dda17ca07d1d7
niall <20103066@setu.ie>
2026-05-10 15:34:55 +0100

    Add cancel to recipe gallery to go back

--- app/src/main/java/org/setu/recipe/activities/RecipeGalleryActivity.kt
+++ app/src/main/java/org/setu/recipe/activities/RecipeGalleryActivity.kt
@@ -2,8 +2,11 @@ package org.setu.recipe.activities
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import org.setu.recipe.R
 import org.setu.recipe.adapters.RecipeGalleryAdapter
 import org.setu.recipe.databinding.ActivityRecipeGalleryBinding
 import org.setu.recipe.databinding.ContentRecipeGalleryBinding
@@ -31,6 +34,23 @@ class RecipeGalleryActivity : AppCompatActivity() {
         contentBinding.galleryRecyclerView.layoutManager = layoutManager
         contentBinding.galleryRecyclerView.adapter = RecipeGalleryAdapter(app.recipes.findAll())
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        menu.findItem(R.id.item_add).isVisible = false
+        menu.findItem(R.id.item_gallery).isVisible = false
+        menu.findItem(R.id.item_delete).isVisible = false
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
